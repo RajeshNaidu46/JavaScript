11ad92c5397d67f8666cdd5dbdf67eaedf1f5bb4
Rajesh Naidu <rajeshnaidu2848@gmail.com>
2025-11-06 21:10:03 +0530

    first commit

---
+++ variables.js
@@ -0,0 +1,15 @@
+ let num = 2+2                        // let is used to declare a variable
+console.log(num);
+console.log(num+10); 
+
+
+let num = 8
+console.log(num);
+
+
+let username = "Rajesh";               // string variable
+console.log(username); 
+
+let username2 = "Rajesh's account"
+console.log(username2);
+
