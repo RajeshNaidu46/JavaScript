50a04dd2bebf0a3ee14f918f51564193013c973e
Rajesh Naidu <rajeshnaidu2848@gmail.com>
2025-11-08 21:51:03 +0530

    more_practice

--- datatypes.js
+++ datatypes.js
@@ -17,3 +17,81 @@ console.log(num2*-1);
 
 console.log(typeof num2); // "number"
 console.log(num2);
+
+
+/// string pratice sesssion using different quotes
+
+let firstName = 'John';
+let lastName = "Doe";
+let username = firstName + " " + lastName;
+console.log(username);  
+
+
+// Working with different string quotes
+const compamyName = "Meta";
+const founding_year = 2000;
+let isActive= "true";
+let fundingAmount 
+console.log("companyName");
+console.log("founding_year");
+console.log(isActive);
+console.log("fundingAmount");
+
+
+
+
+/// boolean datatype practice session
+
+let bool = 5<10;
+console.log(bool); // true  
+
+let bool2 = 10>20;
+console.log(bool2); // false
+
+// null datatype practice session
+
+let nullVar = null;
+console.log(nullVar); // null
+ 
+let user 
+console.log(user); // undefined   
+
+
+const fahrenheit_ = "17";
+let celsius = (fahrenheit_ - 32) / 1.8;
+console.log(celsius);
+
+
+
+// BMI Calculation using exponentiation operator
+
+const weight = 60;
+const height = 6;
+let bmi = weight / (height**2);
+console.log(bmi);
+
+
+//more usage of variables
+
+const salary  = 50000;
+let montlySpending = 35000;
+let savings = salary - montlySpending;
+console.log("Monthly savings are " + savings);  
+
+// Weight conversion earth to mars
+
+const earthWeight = 210;
+const marsWeight = earthWeight * 0.38;
+
+console.log("Your weight on Earth is " + earthWeight + " pounds.");
+console.log("Your weight on Mars is " + marsWeight + " pounds.");
+
+
+let apples = 5;
+let oranges = 15;
+let bananas = 3;
+
+console.log("I have"  +   apples    + "   apples   " );
+console.log( "I have" +  oranges     +  "  oranges  " );
+
+
